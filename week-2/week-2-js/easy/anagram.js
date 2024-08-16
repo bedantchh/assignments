21c049607cdd0f03cc22eea568665055e4e75e47
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-08-16 17:46:21 +0000

    easy anagram done.

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
-
+    let arr1 = str1.split("")
+    let sortedstring1 = arr1.sort()
+    let sortedstr1 = sortedstring1.join("")
+    let arr2 = str1.split("")
+    let sortedstring2 = arr2.sort()
+    let sortedstr2 = sortedstring2.join("")
+    
+    if(sortedstr1 == sortedstr2){
+      return true
+    }
+    else {
+      return false
+    }
 }
+
+let result = isAnagram("hello","lloeh")
+
+console.log(result)
 
 module.exports = isAnagram;
